1fffc3ce88aeae510ebe53ace8acb674d40f53e0
Rohitkumarrai14 <rohit2004bxr@gmail.com>
2026-06-12 12:55:05 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict" // treat all the JS code as newer version of the JS
+
+// alert(3+3)  // we are using node JS not browswer so that's why this give alert
+
+let name="rohit"
+let age=15
+let isloggedIN=false;
+
+//number=> 2 to power 53
+//bigint   if no. is big we use big int 
+//string=>""      use double qotes
+//boolean=>  true/false
+//null => standalone value
+//undefined => value is not defined and variable is created 
+// sybmol =>uniqueness ke lye use hota h 
+
+//objects
+
+console.log(typeof "rohit") //It tell the type of the eg.string
+console.log(typeof undefined)   //undefined 
+console.log(typeof null) // Object Type
+
