3f44a093f53110972f786a95d322f1c4cd8b923b
Rohitkumarrai14 <rohit2004bxr@gmail.com>
2026-06-14 19:00:32 +0000

    conversion&operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,34 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// *******************************Operation****************************
+
+let value=3
+let negvalue= -value  // we just assigned the negative value 
+console.log(negvalue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*8)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)   //basic maths operation 
+
+let str1="hello"
+let str2=" rohit"
+
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1 + "2")
+console.log("1"+2+2)
+console.log( 1+2+"2")
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
