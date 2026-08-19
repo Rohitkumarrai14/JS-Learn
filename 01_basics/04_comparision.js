fe8dcd24757a51a7270be92eac600edba2929f2b
Rohitkumarrai14 <rohit2004bxr@gmail.com>
2026-08-19 09:33:57 +0000

    comparison

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -12,4 +12,4 @@ console.log(undefined==0)// in any case its fasle ,>,<.
 //comparison converts null to number,trating it as 0.that's why null>=0 is true and null>0 is false 
 
 //STRICT CHECK (===)
-console.log("2"===2)
+console.log("2"===2) //It do the complete comparison
